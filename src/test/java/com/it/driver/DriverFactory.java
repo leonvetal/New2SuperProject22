77aac6f88d22da632439bb54e4a10b801f92797b
vetal <leonvetall@gmail.com>
2021-03-13 23:57:24 +0200

    Commit2BrStack

---
+++ src/test/java/com/it/driver/DriverFactory.java
@@ -0,0 +1,66 @@
+package com.it.driver;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import java.net.MalformedURLException;
+import java.net.URL;
+import org.openqa.selenium.edge.EdgeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.ie.InternetExplorerDriver;
+import org.openqa.selenium.remote.DesiredCapabilities;
+import org.openqa.selenium.remote.RemoteWebDriver;
+
+import static com.it.common.Constants.BASE_URL;
+
+public class DriverFactory {
+
+    public static final String AUTOMATE_USERNAME = "bsuser7502162567";
+    public static final String AUTOMATE_ACCESS_KEY = "muHzi1qoJ2ABayF1sbvn";
+    public static final String URL = "https://" + AUTOMATE_USERNAME + ":" + AUTOMATE_ACCESS_KEY + "@hub-cloud.browserstack.com/wd/hub";
+
+    public static WebDriver getDriver()  {
+        WebDriver driver = null;
+
+        String driverName = System.getProperty("driver");
+        if (driverName != null) {
+            if ("fireFox".equals(driverName)) {
+                WebDriverManager.firefoxdriver().setup();
+                driver = new FirefoxDriver();
+            } else if ("ie".equals(driverName)) {
+                WebDriverManager.iedriver().setup();
+                driver = new InternetExplorerDriver();
+            } else if ("edge".equals(driverName)) {
+                WebDriverManager.edgedriver().setup();
+                driver = new EdgeDriver();
+            } else if ("Chrome".equals(driverName)) {
+                WebDriverManager.chromedriver().setup();
+                driver = new ChromeDriver();
+            } else if ("BrSt".equals(driverName)) {
+                DesiredCapabilities caps = new DesiredCapabilities();
+                caps.setCapability("browserName", "Android");
+                caps.setCapability("device", "Samsung Galaxy S9");
+                caps.setCapability("realMobile", "true");
+                caps.setCapability("os_version", "8.0");
+                caps.setCapability("name", "BStack-[Java] Sample Test"); // test name
+                caps.setCapability("build", "BStack Build Number 1"); // CI/CD job or build name
+                try {
+                    driver = new RemoteWebDriver(new URL(URL), caps);
+                } catch (MalformedURLException e) {
+                    e.printStackTrace();
+                }
+
+            }
+            else {
+                throw new UnsupportedOperationException();
+            }
+        } else {
+            WebDriverManager.chromedriver().setup();
+            driver = new ChromeDriver();
+        }
+        driver.manage().window().maximize();
+        driver.get(BASE_URL);
+        return driver;
+    }
+}
--- src/test/java/com/it/pages/BasePage.java
+++ src/test/java/com/it/pages/BasePage.java
@@ -1,5 +1,6 @@
 package com.it.pages;
 
+import com.it.driver.DriverFactory;
 import io.github.bonigarcia.wdm.WebDriverManager;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
@@ -8,13 +9,8 @@ import org.openqa.selenium.support.PageFactory;
 import static com.it.common.Constants.BASE_URL;
 
 public abstract class BasePage {
-    protected static WebDriver driver;
-    static {
-        WebDriverManager.chromedriver().setup();
-        driver = new ChromeDriver();
-        driver.manage().window().maximize();
-        driver.get(BASE_URL);
-    }
+    protected static WebDriver driver= DriverFactory.getDriver();
+
 
     public BasePage() {
         PageFactory.initElements(driver,this);
